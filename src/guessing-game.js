87f2b6ccb67ee4df84c45a7db86e7e2705d01912
nastkas <nastya.by@mail.ru>
2021-09-05 23:05:30 +0300

    feat: create solution of task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor(value) {
+
+                
+    }
 
     setRange(min, max) {
-
+        this.min=min;
+        this.max=max;
     }
 
     guess() {
-
+        this.result = Math.round((this.min + this.max) / 2);
+        return this.result;
     }
 
     lower() {
-
+        this.max = this.result
     }
 
     greater() {
-
+        this.min = this.result
     }
 }
+
 
 module.exports = GuessingGame;
